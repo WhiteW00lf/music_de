36766f951e49512b1d149aeea101ed4fd3fa1d6d
WhiteW00lf <mocha_cookie_crumble@proton.me>
2026-09-25 15:01:13 +0530

    dbt intialized

---
+++ logs/query_log.sql
@@ -0,0 +1,10 @@
+-- created_at: 2026-09-25T09:21:37.170776800+00:00
+-- finished_at: 2026-09-25T09:21:37.559328400+00:00
+-- elapsed: 388ms
+-- outcome: success
+-- dialect: snowflake
+-- node_id: not available
+-- query_id: 01c74dd1-020b-a479-001a-f1c30049096e
+-- desc: not provided
+select 1 as id;
+
